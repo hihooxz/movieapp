4d3e9ea01e7700beb2e6946c1696425a68e208f3
rikyc <rikykhoerul@onoff.insure>
2020-10-16 01:03:12 +0700

    ganti popular

--- data/remote/src/main/java/com/rikyhihooz/remote/utils/Constans.kt
+++ data/remote/src/main/java/com/rikyhihooz/remote/utils/Constans.kt
@@ -11,7 +11,7 @@ object Constans {
     const val TOP_RATE_MOVIE = 2
     const val NOW_PLAYING_MOVIE = 3
 
-    const val POPULER_MOVIE_URL = "28"
+    const val POPULER_MOVIE_URL = "popular"
     const val UPCOMING_MOVIE_URL = "upcoming"
     const val TOP_RATE_MOVIE_URL = "top_rated"
     const val NOW_PLAYING_MOVIE_URL = "now_playing"
